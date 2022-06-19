230f0339772c49b40e41a5c4ff58b0f24fa7c47f
abkaium920 <101384847+abkaium920@users.noreply.github.com>
2022-06-19 18:13:34 +0600

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -1,44 +1,81 @@
 from time import sleep
 from telegram import InlineKeyboardMarkup
 from telegram.message import Message
+from telegram.update import Update
 from telegram.error import RetryAfter
 from pyrogram.errors import FloodWait
 
+from bot import *
 from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, status_reply_dict, status_reply_dict_lock, \
-                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, RSS_CHAT_ID, bot, rss_session
+                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, RSS_CHAT_ID, rss_session, bot
 from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval
 
 
-def sendMessage(text: str, bot, message: Message):
+def sendMessage(text: str, bot, update: Update):
     try:
-        return bot.sendMessage(message.chat_id,
-                            reply_to_message_id=message.message_id,
+        return bot.send_message(update.message.chat_id,
+                            reply_to_message_id=update.message.message_id,
                             text=text, allow_sending_without_reply=True, parse_mode='HTMl', disable_web_page_preview=True)
     except RetryAfter as r:
         LOGGER.warning(str(r))
         sleep(r.retry_after * 1.5)
-        return sendMessage(text, bot, message)
+        return sendMessage(text, bot, update)
     except Exception as e:
         LOGGER.error(str(e))
         return
 
-def sendMarkup(text: str, bot, message: Message, reply_markup: InlineKeyboardMarkup):
+def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
     try:
-        return bot.sendMessage(message.chat_id,
-                            reply_to_message_id=message.message_id,
+        return bot.send_message(update.message.chat_id,
+                            reply_to_message_id=update.message.message_id,
                             text=text, reply_markup=reply_markup, allow_sending_without_reply=True,
                             parse_mode='HTMl', disable_web_page_preview=True)
     except RetryAfter as r:
         LOGGER.warning(str(r))
         sleep(r.retry_after * 1.5)
-        return sendMarkup(text, bot, message, reply_markup)
+        return sendMarkup(text, bot, update, reply_markup)
     except Exception as e:
         LOGGER.error(str(e))
         return
 
+def sendLog(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
+    try:
+        return bot.send_message(f"{LOG_CHANNEL}",
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, reply_markup=reply_markup, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+def sendtextlog(text: str, bot, update: Update):
+    try:
+        return bot.send_message(f"{LOG_SEND_TEXT}",
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+def sendPrivate(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
+    bot_d = bot.get_me()
+    b_uname = bot_d.username
+
+    try:
+        return bot.send_message(update.message.from_user.id,
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, reply_markup=reply_markup, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+        if "Forbidden" in str(e):
+            uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+            botstart = f"http://t.me/{b_uname}?start=start"
+            keyboard = [
+            [InlineKeyboardButton("START BOT", url = f"{botstart}")],
+            [InlineKeyboardButton("JOIN LOG CHANNEL", url = f"t.me/{LOG_UNAME}")]]
+            sendMarkup(f"Dear {uname},\n\n<b>I Found That You Haven't Started Me In PM (Private Chat) Yet.</b>\n\n<b>From Now On I Will Give You Links In PM (Private Chat) Only.</b>\n\n<i><b>Please Start Me in PM (Private Chat) & Don't Miss Future Uploads.</b></i>\n\n<b>From Now Get Your Links From @{LOG_UNAME} Or Search Using /search</b>.", bot, update, reply_markup=InlineKeyboardMarkup(keyboard))
+            return
+
 def editMessage(text: str, message: Message, reply_markup=None):
     try:
-        bot.editMessageText(text=text, message_id=message.message_id,
+        bot.edit_message_text(text=text, message_id=message.message_id,
                               chat_id=message.chat.id,reply_markup=reply_markup,
                               parse_mode='HTMl', disable_web_page_preview=True)
     except RetryAfter as r:
@@ -52,7 +89,7 @@ def editMessage(text: str, message: Message, reply_markup=None):
 def sendRss(text: str, bot):
     if rss_session is None:
         try:
-            return bot.sendMessage(RSS_CHAT_ID, text, parse_mode='HTMl', disable_web_page_preview=True)
+            return bot.send_message(RSS_CHAT_ID, text, parse_mode='HTMl', disable_web_page_preview=True)
         except RetryAfter as r:
             LOGGER.warning(str(r))
             sleep(r.retry_after * 1.5)
@@ -62,11 +99,10 @@ def sendRss(text: str, bot):
             return
     else:
         try:
-            with rss_session:
-                return rss_session.send_message(RSS_CHAT_ID, text, disable_web_page_preview=True)
+            return rss_session.send_message(RSS_CHAT_ID, text, parse_mode='HTMl', disable_web_page_preview=True)
         except FloodWait as e:
             LOGGER.warning(str(e))
-            sleep(e.value * 1.5)
+            sleep(e.x * 1.5)
             return sendRss(text, bot)
         except Exception as e:
             LOGGER.error(str(e))
@@ -74,16 +110,16 @@ def sendRss(text: str, bot):
 
 def deleteMessage(bot, message: Message):
     try:
-        bot.deleteMessage(chat_id=message.chat.id,
+        bot.delete_message(chat_id=message.chat.id,
                            message_id=message.message_id)
     except Exception as e:
         LOGGER.error(str(e))
 
-def sendLogFile(bot, message: Message):
+def sendLogFile(bot, update: Update):
     with open('log.txt', 'rb') as f:
-        bot.sendDocument(document=f, filename=f.name,
-                          reply_to_message_id=message.message_id,
-                          chat_id=message.chat_id)
+        bot.send_document(document=f, filename=f.name,
+                          reply_to_message_id=update.message.message_id,
+                          chat_id=update.message.chat_id)
 
 def auto_delete_message(bot, cmd_message: Message, bot_message: Message):
     if AUTO_DELETE_MESSAGE_DURATION != -1:
@@ -109,10 +145,15 @@ def update_all_messages():
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
-                if buttons == "":
-                    editMessage(msg, status_reply_dict[chat_id])
-                else:
-                    editMessage(msg, status_reply_dict[chat_id], buttons)
+                if buttons(msg) == "":
+                    msg = "Starting DL"
+                try:
+                    keyboard = [[InlineKeyboardButton("REFRESH", callback_data=str(ONE)),
+                                 InlineKeyboardButton("CLOSE", callback_data=str(TWO)),],
+                                [InlineKeyboardButton("STATS", callback_data=str(THREE)),]]
+                    editMessage(msg, status_reply_dict[chat_id], reply_markup=InlineKeyboardMarkup(keyboard))
+                except Exception as e:
+                    LOGGER.error(str(e))
                 status_reply_dict[chat_id].text = msg
 
 def sendStatusMessage(msg, bot):
@@ -120,16 +161,69 @@ def sendStatusMessage(msg, bot):
         Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
     progress, buttons = get_readable_message()
     with status_reply_dict_lock:
-        if msg.chat.id in list(status_reply_dict.keys()):
+        if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
-                message = status_reply_dict[msg.chat.id]
+                message = status_reply_dict[msg.message.chat.id]
                 deleteMessage(bot, message)
-                del status_reply_dict[msg.chat.id]
+                del status_reply_dict[msg.message.chat.id]
             except Exception as e:
                 LOGGER.error(str(e))
-                del status_reply_dict[msg.chat.id]
-        if buttons == "":
+                del status_reply_dict[msg.message.chat.id]
+        if buttons(progress) == "":
+            progress = "Starting DL"
             message = sendMessage(progress, bot, msg)
         else:
             message = sendMarkup(progress, bot, msg, buttons)
-        status_reply_dict[msg.chat.id] = message
+        status_reply_dict[msg.message.chat.id] = message
+
+ONE, TWO, THREE = range(3)
+
+def refresh(update, context):
+    query = update.callback_query
+    query.edit_message_text(text="Refreshing Status...⏳")
+    time.sleep(2)
+    update_all_messages()
+
+def close(update, context):
+    chat_id  = update.effective_chat.id
+    user_id = update.callback_query.from_user.id
+    bot = context.bot
+    query = update.callback_query
+    admins = bot.get_chat_member(chat_id, user_id).status in ['creator', 'administrator'] or user_id in [OWNER_ID]
+    if admins:
+        delete_all_messages()
+    else:
+        query.answer(text="You Don't Have Admin Rights!", show_alert=True)
+
+def pop_up_stats(update, context):
+    query = update.callback_query
+    stats = bot_sys_stats()
+    query.answer(text=stats, show_alert=True)
+
+def bot_sys_stats():
+    currentTime = get_readable_time(time.time() - botStartTime)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    total, used, free = shutil.disk_usage('.')
+    total = get_readable_file_size(total)
+    used = get_readable_file_size(used)
+    free = get_readable_file_size(free)
+    recv = get_readable_file_size(psutil.net_io_counters().bytes_recv)
+    sent = get_readable_file_size(psutil.net_io_counters().bytes_sent)
+    stats = f"""
+🔹𝙱𝙾𝚃 𝚄𝙿𝚃𝙸𝙼𝙴 🕐 : {currentTime}
+🔸𝙲𝙿𝚄 : {progress_bar(cpu)} {cpu}%
+🔹𝚁𝙰𝙼  : {progress_bar(mem)} {mem}%
+🔸𝙳𝙸𝚂𝙺 : {progress_bar(disk)} {disk}%
+🔹𝚃𝙾𝚃𝙰𝙻 : {total}
+🔸𝚄𝚂𝙴𝙳 : {used} || FREE : {free}
+🔹𝚂𝙴𝙽𝚃 : {sent} || RECV : {recv}
+
+⚡𝙿𝚘𝚠𝚍𝚎𝚛𝚎𝚍 𝙱𝚢 @AB_KAIUM⚡
+"""
+    return stats
+
+dispatcher.add_handler(CallbackQueryHandler(refresh, pattern='^' + str(ONE) + '$'))
+dispatcher.add_handler(CallbackQueryHandler(close, pattern='^' + str(TWO) + '$'))
+dispatcher.add_handler(CallbackQueryHandler(pop_up_stats, pattern='^' + str(THREE) + '$'))
